527ea60c96ed4879489f773861f38ed074325764
SDC <99742065+U-F-Os@users.noreply.github.com>
2022-11-16 14:42:58 -0500

    Segundo proyecto con Django

--- Proyecto2/Proyecto2/settings.py
+++ Proyecto2/Proyecto2/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'articulosclientes',
         'USER': 'postgres',
-        'PASSWORD': '', # Contraseña de la BD
+        'PASSWORD': '', # Contraseña de la base de datos.
         'HOST': '127.0.0.1',
         'DATABASE_PORT': '5432',
     }
